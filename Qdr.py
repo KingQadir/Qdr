a597d3bc3a05f10d83543dc340a03d9119dede98
Abdul Qadir <110684636+KingQadir@users.noreply.github.com>
2023-06-22 00:25:40 +0500

    Update Qdr.py

--- Qdr.py
+++ Qdr.py
@@ -1,1 +1,21 @@
+import os, platform
 
+os.system("git pull")
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+	from Qdr import Fire	Fire()
+
+elif bit == '32bit':
+
+	from Qdr32 import Fire
+
+	Fire()
+
+else:
+
+	print('\n\x1b[1;91m[×] Your Device is Not Supported This Tool !');exit()
+
+        
